531b0808aee19d7c5a9b06224d2994c73d9d6862
kaiwen <kaiwen.yuan@intel.com>
2023-08-07 14:20:15 +0800

    add function of 'Update' and add listener for database sension

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 import json
+import datetime
 from flask import Flask, render_template, request, redirect, flash, Response, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import and_, or_
+from sqlalchemy import and_, or_, event
+from sqlalchemy.exc import SQLAlchemyError
+
+
+# TODO: PostgreSQL in Server
+# TODO: 整理代码，统一接口
+
+def get_formated_time():
+    current_time = datetime.datetime.now()
+    return current_time.strftime('%Y-%m-%d %H:%M:%S')
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///c:\\sqlite3\\asset.db'
 app.secret_key = '\xc9ixnRb\xe40\xd4\xa5\x7f\x03\xd0y6\x01\x1f\x96\xeao+\x8a\x9f\xe4'
 db = SQLAlchemy(app)
+
+log_str = ""
 
 
 # 新建模型 用户、管理员、资产的ORM
@@ -17,14 +30,14 @@ class User(db.Model):  # 表名将会是 user（自动生成，小写）
     password = db.Column(db.String(80))
 
 
-class Admin(db.Model):  # 表名将会是 admin（自动生成，小写）
+class Admin(db.Model):  # table admin
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     email = db.Column(db.String(60))
     password = db.Column(db.String(80))
 
 
-class Asset(db.Model):
+class Asset(db.Model):  # table asset
     AssetID = db.Column(db.String, primary_key=True)
     Type = db.Column(db.String)
     Status = db.Column(db.String)
@@ -49,11 +62,6 @@ class Asset(db.Model):
     Bandwidth = db.Column(db.Integer)
 
 
-# with app.app_context():
-#     db.create_all()
-#     db.session.commit()
-
-
 def user_login(username, password):
     user = User.query.filter(and_(User.name == username, User.password == password)).first()
     if user:
@@ -68,6 +76,27 @@ def admin_login(email, password):
         return True
     else:
         return False
+
+
+@event.listens_for(db.session, 'before_flush')
+def log_changes(session, flush_context, instances):
+    global log_str
+    log_str = ""
+    for obj in session.new:
+        if isinstance(obj, Asset):
+            log_str = f"Add asset_id: {obj.AssetID}"
+    for obj in session.deleted:
+        if isinstance(obj, Asset):
+            if log_str:
+                log_str = f"Deleted asset_id: {obj.AssetID}"
+            else:
+                log_str += f",Deleted asset_id: {obj.AssetID}"
+    for obj in session.dirty:
+        if isinstance(obj, Asset):
+            log_str = f"Update asset_id: {obj.AssetID}"
+        for attr in db.inspect(obj).attrs:
+            if attr.history.has_changes():
+                log_str += f" change {attr.key} from {attr.history.deleted[0]} to {attr.value}"
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -94,8 +123,33 @@ def forgotPassword():
     return render_template('auth-cover-forgot-password.html')
 
 
+# 添加新用户
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['Username']
+        email = request.form['Email']
+        password = request.form['Password']
+        # 判断用户是否已经存在
+        existing_user = Admin.query.filter_by(email=email).first()
+        if existing_user:
+            response = {
+                "status": "failed",
+                "message": "User with this email already exists. Please choose a different email."
+            }
+            return jsonify(response)
+        # 创建向数据库添加的新用户信息
+        new_user = Admin()
+        new_user.email = email
+        new_user.name = username
+        new_user.password = password
+        db.session.add(new_user)
+        db.session.commit()
+        response = {
+            "status": "success",
+            "message": "Registration successful!"
+        }
+        return jsonify(response)
     return render_template('auth-cover-register.html')
 
 
@@ -105,8 +159,16 @@ def admin():
     try:
         # 将所有列名存到列表里
         all_columns = [column.key for column in Asset.__table__.columns]
-        # 查询"Asset"表的所有数据
-        all_assets = Asset.query.all()
+        # 如果前端传输了数据的筛选条件
+        if request.method == 'POST':
+            # 从表单中获取用户输入
+            # search_query = request.form['search_query']
+            # TODO 使用筛选条件查询数据库（按照特定列进行筛选，例如“asset_name”）
+            # filtered_assets = Asset.query.filter(Asset.asset_name.like(f"%{search_query}%")).all()
+            all_assets = query_record()
+        else:
+            # 如果是GET请求或未提供输入，则获取所有资产信息
+            all_assets = Asset.query.all()
         # 将查询到的数据转换为列表的列表格式
         table_data = []
         for asset in all_assets:
@@ -124,23 +186,28 @@ def user():
     return render_template('user.html')
 
 
-# 向数据库添加记录的方法
+# TODO: try-except
 @app.route('/add_record', methods=['POST'])
 def add_record():
     data = request.json        # 获取json格式的数据
+    # TODO: add other attributes
     new_asset = Asset()
     new_asset.AssetID = data.get('AssetID')
-    new_asset.Type = data.get('Type')
+    new_asset.Type = data.get('Type', '')
+    change_time = get_formated_time()
+    new_asset.ChangeTime = change_time
     db.session.add(new_asset)
     db.session.commit()
+
+    # TODO: add into log table
+    print(log_str)
 
     # 整合数据成json格式
     data = {'message': 'Records are added successfully'}
     return Response(json.dumps(data), status=200, mimetype='application/json')
 
-
+@app.route('/query_asset', methods=['POST'])
 # 根据条件筛选查询数据库记录
-@app.route('/query_record', methods=['POST'])
 def query_record():
     data = request.json
     type_ = data.get('Type', None)
@@ -177,7 +244,7 @@ def query_record():
             'Owner': asset_.Owner,
             'ProjectID': asset_.ProjectID,
             'Project': asset_.Project,
-            'Rack':asset_.Rack,
+            'Rack': asset_.Rack,
             'BarCode': asset_.BarCode,
             'SN': asset_.SN,
             'Model': asset_.Model,
@@ -198,9 +265,59 @@ def query_record():
     return Response(jsonify(asset_list), status=200, mimetype='application/json')
 
 
-# 根据选择删除数据库记录
-# def del_record():
+@app.route('/update_asset', methods=['POST'])
+def update_record():
+    try:
+        data = request.json
+        AssetID = data.get("AssetID")
+        record = Asset.query.filter_by(AssetID=AssetID).first()
+        # TODO: add more attributes
+        comments = data.get("Comments", "")
+        if comments:
+            record.Comments = comments
+        change_time = get_formated_time()
+        record.ChangeTime = change_time
+        db.session.commit()
 
+        # TODO: add into log table
+        userID = data.get('userID')
+        print(log_str)
+
+        return jsonify({'message': '记录已成功更新'})
+    except SQLAlchemyError as e:
+        db.session.rollback()  # 回滚事务
+        return jsonify({'message': '更新失败：数据库错误'}), 500
+
+    except Exception as e:
+        return jsonify({'message': '更新失败：' + str(e)}), 500
+
+
+@app.route('/delete_asset', methods=['POST'])
+def delete_record():
+    try:
+        data = request.json
+        assetid_list = data.get('assetid_list', [])
+        if not assetid_list:
+            return jsonify({'message': '删除失败，未选中信息'}), 400
+        records = Asset.query.filter(Asset.AssetID.in_(assetid_list)).all()
+        for record in records:
+            db.session.delete(record)
+        db.session.commit()
+
+        # TODO: add into log table
+        userID = data.get('userID')
+        change_time = get_formated_time()
+        log_list = log_str.split(',')
+        for log in log_list:
+            print(log)
+
+        return jsonify({'message': '记录已成功删除'})
+    except SQLAlchemyError as e:
+        db.session.rollback()  # 回滚事务
+        return jsonify({'message': '删除失败：数据库错误'}), 500
+
+    except Exception as e:
+        return jsonify({'message': '删除失败：' + str(e)}), 500
 
 
 if __name__ == '__main__':
--- test/test.py
+++ test/test.py
@@ -1,46 +1,21 @@
-# -*- coding: utf-8 -*-
 import requests
+import time
 
-# 定义API端点和请求数据
-api_url = 'http://127.0.0.1:5000/add_record'
-data = {
-    'AssetID': 'HD000480',
-    'Type': 'test',
-}
 
-#  发送POST请求调用API
-response = requests.post(api_url, json=data)
+def woshishabi():
+    print("wo bu shi sha bi")
 
-# 检查响应状态码
+# 定义要测试的 URL
+url = 'http://localhost:5000/update_asset'
+
+# 准备要发送的数据，以 JSON 格式
+data = {'AssetID': 'test03', 'Comments': 'hhh'}  # 假设要删除的记录的 ID 列表
+
+# 发送 POST 请求
+response = requests.post(url, json=data)
+
+# 处理响应
 if response.status_code == 200:
-    # 提取并处理响应数据
-
-    # 处理结果
-    print(result)
+    print('删除成功:', response.json())
 else:
-    print('API调用失败')
-
-def get_db():
-    conn = sqlite3.connect(DATABASE)
-    return conn
-def get_table_columns(table_name):
-    conn = get_db()
-    cursor = conn.execute(f"PRAGMA table_info({table_name})")
-    columns = [column[1] for column in cursor.fetchall()]
-    conn.close()
-    return columns # 获取所有表单数据
-    def get_table_data():
-    conn = get_db()
-    cursor = conn.execute('SELECT * FROM assets')
-    data = cursor.fetchall()
-    conn.close()
-    return data
-
-
-@app.route('/')
-def index():
-    table_data = get_table_data()
-    table_columns = get_table_columns('my_table')
-    return render_template('ecommerce-products.html', table_data=table_data, table_columns=table_columns)
-
-    if __name__ == '__main__':    app.run()
+    print('请求失败:', response.status_code, response.text)
